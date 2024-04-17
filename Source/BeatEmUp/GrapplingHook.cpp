af09674f7cf06825fc6644d8565c2f6f04370fdd
Tony <ihsi0002@student.monash.edu>
2024-04-17 23:40:37 +1000

    Rework the grapling hook to LineTrace to Projectile Scaning

--- Source/BeatEmUp/GrapplingHook.cpp
+++ Source/BeatEmUp/GrapplingHook.cpp
@@ -4,31 +4,35 @@
 #include "GrapplingHook.h"
 
 #include "BeatEmUpCharacter.h"
+#include "Enemy.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AGrapplingHook::AGrapplingHook() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	RootComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Hook Mesh"));
-	CableComponent = CreateDefaultSubobject<UCableComponent>(TEXT("Cable"));
+	HookMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Hook Mesh"));
+	HookMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	HookMesh->SetCollisionResponseToAllChannels(ECR_Block);
+	HookMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+	HookMesh->OnComponentHit.AddDynamic(this, &AGrapplingHook::OnHit);
+	RootComponent = HookMesh;
 	
+	CableComponent = CreateDefaultSubobject<UCableComponent>(TEXT("Cable"));
 	CableComponent->SetupAttachment(RootComponent);
 	CableComponent->SetVisibility(true);
-
 	CableComponent->CableLength = 1;
 	CableComponent->NumSegments = 1;
-	CableComponent->CableWidth = 0.1;
-}
+	CableComponent->CableWidth = 2;
 
-void AGrapplingHook::Launch(const FVector& NewTargetLocation, AActor* NewInitiator) {
-	TargetLocation = NewTargetLocation;
-	Initiator = NewInitiator;
-	
-	if(Initiator == nullptr) return;
-	if(CableComponent == nullptr) return;
-
-	CableComponent->SetAttachEndToComponent(Initiator->GetRootComponent());
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	ProjectileMovement->UpdatedComponent = RootComponent;
+	ProjectileMovement->InitialSpeed = InitFlingSpeed;
+	ProjectileMovement->MaxSpeed = MaxFlyingSpeed;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->ProjectileGravityScale = 0.f;
 }
 
 // Called when the game starts or when spawned
@@ -42,7 +46,7 @@ void AGrapplingHook::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	if(!bRetracting) {
-		SetActorLocation(FMath::VInterpConstantTo(GetActorLocation(), TargetLocation, DeltaTime, FlyingSpeed));
+		// SetActorLocation(FMath::VInterpConstantTo(GetActorLocation(), TargetLocation, DeltaTime, MaxFlyingSpeed));
 		if(FVector::Dist(GetActorLocation(), TargetLocation) < AttachThreshold)
 			bRetracting = true;
 	}
@@ -68,4 +72,31 @@ void AGrapplingHook::Tick(float DeltaTime)
             Destroy();
         }
 	}
+}
+
+void AGrapplingHook::Launch(const FVector& NewTargetLocation, AActor* NewInitiator) {
+	TargetLocation = NewTargetLocation;
+	Initiator = NewInitiator;
+	
+	if(Initiator == nullptr) return;
+	if(CableComponent == nullptr) return;
+
+	CableComponent->SetAttachEndToComponent(Initiator->GetRootComponent());
+
+	const FVector LaunchDirection = (TargetLocation - GetActorLocation()).GetSafeNormal();
+	ProjectileMovement->Velocity = LaunchDirection * ProjectileMovement->InitialSpeed;
+}
+
+void AGrapplingHook::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit) {
+	if(OtherActor == Initiator) return;
+	
+	if (AEnemy* HitEnemy = Cast<AEnemy>(OtherActor)) {
+		UE_LOG(LogTemp, Warning, TEXT("OnHit() Called by %s"), *OtherActor->GetName());
+		AttachToComponent(OtherComp, FAttachmentTransformRules::KeepWorldTransform);
+	}
+	else if (OtherComp && OtherComp->IsSimulatingPhysics()) {
+		AttachToComponent(OtherComp, FAttachmentTransformRules::KeepWorldTransform);
+	}
+	
 }
