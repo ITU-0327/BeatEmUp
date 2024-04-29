d6959d98e0e489d34ce0cf87efdb89ba3beeccb4
Tony <ihsi0002@student.monash.edu>
2024-04-30 02:02:08 +1000

    Added Jump Points for AI to navigate between nav mesh

---
+++ Source/BeatEmUp/JumpPoint.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JumpPoint.h"
+
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+
+void AJumpPoint::BeginPlay() {
+	Super::BeginPlay();
+
+	OnSmartLinkReached.AddDynamic(this, &AJumpPoint::LinkReached);
+	CopyEndPointsFromSimpleLinkToSmartLink();
+}
+
+void AJumpPoint::LinkReached(AActor* Agent, const FVector& Destination) {
+	ACharacter* Enemy = Cast<ACharacter>(Agent);
+	if(!Enemy) return;
+
+	FVector LaunchVelocity;
+	UGameplayStatics::SuggestProjectileVelocity_CustomArc(GetWorld(), LaunchVelocity, Enemy->GetActorLocation(), Destination);
+	Enemy->LaunchCharacter(LaunchVelocity * 1.1, true, true);
+}
---
+++ Source/BeatEmUp/JumpPoint.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navigation/NavLinkProxy.h"
+#include "JumpPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATEMUP_API AJumpPoint : public ANavLinkProxy
+{
+	GENERATED_BODY()
+
+	virtual void BeginPlay() override;
+	UFUNCTION()
+		void LinkReached(AActor* Agent, const FVector& Destination);
+};
