07115ad422f38ee5ebb42557be9a7e89022bd379
Tony <ihsi0002@student.monash.edu>
2024-05-07 23:40:35 +1000

    Create a Struct for Frame Data.

---
+++ Source/BeatEmUp/FrameData.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "FrameData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FFrameData {
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Frame Data")
+	FVector Position;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Frame Data")
+	FRotator Rotation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Frame Data")
+	float Timestamp;
+
+	FFrameData() : Position(FVector::ZeroVector), Rotation(FRotator::ZeroRotator), Timestamp(0.0f) {}
+
+	FFrameData(FVector InPosition, FRotator InRotation, float InTimestamp)
+		: Position(InPosition), Rotation(InRotation), Timestamp(InTimestamp) {}
+};
