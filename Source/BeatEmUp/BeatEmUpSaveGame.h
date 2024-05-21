0b196af577e894c9bb14348aca9e680c15bd3edf
Tony <ihsi0002@student.monash.edu>
2024-05-21 22:31:51 +1000

    Add a Save Game class with all the variable needs to be saved

---
+++ Source/BeatEmUp/BeatEmUpSaveGame.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "BeatEmUpSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATEMUP_API UBeatEmUpSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+		FString SaveSlotName;
+	UPROPERTY()
+		uint32 UserIndex;
+
+	// Player data
+	UPROPERTY()
+		FVector PlayerLocation;
+	UPROPERTY()
+		FRotator PlayerRotation;
+	UPROPERTY()
+		float PlayerCurrentHealth;
+	UPROPERTY()
+		float PlayerMaxHealth;
+	UPROPERTY()
+		float PlayerCurrentExp;
+	UPROPERTY()
+		float PlayerExpToLevel;
+	UPROPERTY()
+		float PlayerPunchDamage;
+
+	// Enemy data
+	UPROPERTY()
+		TArray<FVector> EnemyLocations;
+	UPROPERTY()
+		TArray<FRotator> EnemyRotations;
+	UPROPERTY()
+		TArray<float> EnemyCurrentHealths;
+	UPROPERTY()
+		TArray<float> EnemyMaxHealths;
+	UPROPERTY()
+		TArray<bool> EnemyRagdollStates;
+	UPROPERTY()
+		TArray<FVector> EnemyMeshLocations;
+	UPROPERTY()
+		TArray<FRotator> EnemyMeshVelocities;
+};
